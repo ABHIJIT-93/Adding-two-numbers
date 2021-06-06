6921c68bf0839d94f8e7ef0f9e423096217cfaa4
ABHIJIT-93 <72138757+ABHIJIT-93@users.noreply.github.com>
2021-06-06 16:18:36 +0530

    commenting the lines of code

--- ADDITION OF TWO NUMBER.c
+++ ADDITION OF TWO NUMBER.c
@@ -2,11 +2,12 @@
 #include<conio.h>
 int main()
 {
-    int a,b,c;
-    printf("enter two numbers \n");
-    scanf("%d%d",&a,&b);
-    c=a+b;
-    printf("sum of two number is %d",c);
-    getch();
+    int a,b,c;                                  //initializing the variables//
+    printf("enter two numbers \n");             //entering number from the user//
+    scanf("%d%d",&a,&b);                        //assigning value taken by the user in the variable//
+    c=a+b;                                      //This is the formula//
+    printf("sum of two number is %d",c);        //printing results//
+    return 0;                                   //returning 0//
+    getch();                                    //taking input from the user//
 
 }
