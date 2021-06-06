bf2204aa554cafe270caf8d4cec90a51b1e17b7d
ABHIJIT-93 <72138757+ABHIJIT-93@users.noreply.github.com>
2021-06-06 16:05:16 +0530

    Add files via upload

---
+++ ADDITION OF TWO NUMBER.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter two numbers \n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of two number is %d",c);
+    getch();
+
+}
